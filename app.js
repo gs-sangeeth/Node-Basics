99c5f87096aea38c7d1c16e73b9c7036b6e10940
gs-sangeeth <gssangeeth16@gmail.com>
2021-05-16 12:15:22 +0530

    Http Module basics

--- app.js
+++ app.js
@@ -6,3 +6,20 @@ logger.on("logging", (arg) => {
 });
 
 logger.log("Oh hi Mark!!");
+
+// Http Module
+
+const http = require("http");
+const server = http.createServer((req, res) => {
+  if (req.url == "/") {
+    res.write("Think Mark ! Think!");
+    res.end();
+  }
+});
+
+// server.on("connection", (socket) => {
+//   console.log("Connected!!");
+// });
+
+server.listen(3000);
+console.log("Listening to port 3000...");
